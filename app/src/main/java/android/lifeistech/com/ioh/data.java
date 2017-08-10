4f642dad4e42eae4a3b46543383e21c510456c59
史也　 内山 <fumiyauchiyama@S-Fumiya-Uchiyama-no-MacBook-Air.local>
2017-08-10 16:50:05 +0900

    8/6

--- app/src/main/java/android/lifeistech/com/ioh/data.java
+++ app/src/main/java/android/lifeistech/com/ioh/data.java
@@ -6,9 +6,24 @@ package android.lifeistech.com.ioh;
 
 public class data {
 
-    int month;
-    int day;
-    int time;
-    int guess;
-    boolean eva;
+    public int month;
+    public int day;
+    public int time;
+    public int bigin;
+    public int finish;
+    public String weather;
+    public String memo;
+
+    public data(int month,int day,int time,int bigin,int finish,String weather,String memo){
+
+        this.month = month;
+        this.day = day;
+        this.time = time;
+        this.bigin =bigin;
+        this.finish = finish;
+        this.weather = weather;
+        this.memo = memo;
+
+    }
+
 }
---
+++ app/src/main/java/android/lifeistech/com/ioh/dataAdapter.java
@@ -0,0 +1,52 @@
+package android.lifeistech.com.ioh;
+
+import android.content.Context;
+import android.icu.util.Calendar;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by fumiyauchiyama on 2017/08/08.
+ */
+
+public class dataAdapter extends ArrayAdapter<data> {
+
+    List<data> mdatas;
+    Calendar cal = Calendar.getInstance();
+
+
+    public dataAdapter(Context context, int layoutResourseId,List<data> objects){
+
+        super(context,layoutResourseId,objects);
+        mdatas = objects;
+
+    }
+
+    public int getount(){
+
+        return mdatas.size();
+
+    }
+
+    public data getItem(int position){
+
+        return mdatas.get(position);
+
+    }
+
+    public int getContains(){
+
+        int i = cal.get(Calendar.DATE);
+
+        return mdatas.contains(i);
+
+    }
+
+    public int getCount(){
+
+        return mdatas.size();
+
+    }
+
+}
